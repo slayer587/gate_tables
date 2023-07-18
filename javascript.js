0af2ce91c806478166ab2f7b0d4145c7a9f999cf
slayer587 <ak587mishra@gmail.com>
2023-07-18 08:20:35 +0530

    Add G:\My Drive\Gate 2024\Notes\javascript.js

---
+++ javascript.js
@@ -0,0 +1,17 @@
+<script>
+  // Function to handle click and toggle description visibility
+  function toggleDescription() {
+    var descriptionContent = this.nextElementSibling;
+    if (descriptionContent.style.display === "none") {
+      descriptionContent.style.display = "block";
+    } else {
+      descriptionContent.style.display = "none";
+    }
+  }
+
+  // Attach the click event to the summary text/button
+  var descriptionSummaries = document.querySelectorAll(".description-summary");
+  descriptionSummaries.forEach(function (summary) {
+    summary.addEventListener("click", toggleDescription);
+  });
+</script>
